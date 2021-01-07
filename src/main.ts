f61ed39aa78fcb312d88709401e314a710a56aee
minhblues <minhblues@minhbluess-MacBook-Pro.local>
2021-01-08 02:37:47 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino-logger';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
-
+import { join } from 'path';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {cors:true});
@@ -15,7 +15,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-
+  app.useStaticAssets(join(__dirname, '..', 'static'));
   await app.listen(process.env.PORT || 8000, '0.0.0.0', () => {
     logger.log('Listening on port 8000');
   });
